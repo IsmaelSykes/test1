660da43c874159a59e3ed8ef9424bb104afacbf7
IsmaelSykes <ismaelbarrera26@gmail.com>
2023-03-22 14:02:17 -0600

    Add lib1.h

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-
+#include "lib1/lib1.h"
 int main(void)
 {
 	int n1 = 1, n2 = 2, r = 0;
 	printf("----- Numbers ----\n");
 	printf("n1: %d \n",n1);
 	printf("n2: %d \n",n2);
+	adding(n1,n2,&r);
+	printf("Adding: %d \n",r);
 
 	return 0;
 }
